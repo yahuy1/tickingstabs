1731f74e4eaac06994d5a4d88b32b9e32a5f12c2
yahuy1 <nguyengiahuyak29@gmail.com>
2022-11-14 00:41:29 -0500

    result animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Result from "./components/Result";
 import {useEffect, useRef, useState} from 'react';
 
 //const text = "If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.";
-const text = "another head into could govern do between seem much open so how at say to not life when he some public first seem point move high school into way other ask";
+const text = "another head"// into could govern do between seem much open so how at say to not life when he some public first seem point move high school into way other ask";
 
 export default function App() {
 	const wordInput = useRef(null); // Reference to the input field
--- src/components/Result.js
+++ src/components/Result.js
@@ -8,12 +8,26 @@ export default function Result(props) {
     useEffect(() => {ref.current.focus();})
 
     useEffect(() => {
+        if (props.score === Infinity) {
+            setScoreCounter(Infinity);
+            return;
+        }
 
+        let interval = setInterval(() => {
+            setScoreCounter(prevState => {
+                if (prevState === props.score) {
+                    clearInterval(interval);
+                }
+                return prevState + 1;
+            });
+        }, Math.floor(500 / props.score));
+
+        return () => clearInterval(interval);
     }, [])
 
     return (
         <div className="result" tabIndex="0" ref={ref} onKeyDown={props.handleModifierKeys}>
-            {props.score}
+            {scoreCounter}
         </div>
     );
 }
