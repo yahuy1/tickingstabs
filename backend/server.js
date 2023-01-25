0886c25ff2f54d3fba7311d197ff9652949ff213
yahuy1 <nguyengiahuyak29@gmail.com>
2023-01-24 21:16:05 -0500

    MongoDB

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`)
+        })
+    })
+    .catch((err) => console.log(err))
